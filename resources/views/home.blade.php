5fe7518be8641b63bf600458bf63cf2b547790f1
ahmad faizal <cyberkind64@gmail.com>
2023-10-07 09:40:53 +0700

    faizal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,20 +8,23 @@
 </head>
 <link rel="stylesheet" href="css/bootstrap.css">
 <link rel="stylesheet" href="ppm.css">
+
 <body>
     {{-- awal navbar --}}
-    <nav class="navbar navbar-expand-lg bg-body-tertiary-color">
+    
+    <div class="shadow-lg p-2 mb-2 bg-body-tertiary rounded" style="padding:50%">
+    <nav class="navbar navbar-expand-lg bs-primary border-subtle">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">PPM</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
+          
           <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
               <a class="nav-link active" aria-current="page" href="#">Home</a>
-              <a class="nav-link" href="pengaduan">Pengaduan</a>
-              <a class="nav-link" href="#">Petugas</a>
-              <a class="nav-link" href="#">Tanggapan</a>
+              <a class="nav-link" href="pengaduan">Login</a>
+              <a class="nav-link" href="#">Validasi</a>
               <a class="nav-link" href="#">Laporan</a>
              {{-- akhir navbar --}}
 
@@ -30,7 +33,8 @@
           </div>
         </div>
       </nav>
-
+    </div>
+   
       @yield('isi')
 </body>
 </html>
