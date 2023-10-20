7eb58a77f87ad9365699e9c9fda26d46095ef8b0
JOEL <cyberkind64@gmail.com>
2023-10-20 12:49:11 +0700

    ganti

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('Masyarakat.home');
 });
 
 Route::get('registrasi',[MasyarakatControll::class,'registrasi']);
